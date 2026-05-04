8b303fd4ea1a2af3d1477bb9e14bfbf4f23d2d86
rakkateichou <rakkateichou@gmail.com>
2026-05-04 23:53:11 +0800

    flask run fix

--- app.py
+++ app.py
@@ -90,7 +90,8 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5001)
