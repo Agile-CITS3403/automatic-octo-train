ee23519b34fc226e10f8c75d6d2952b641793424
rakkateichou <rakkateichou@gmail.com>
2026-05-07 00:47:00 +0800

    Update database schema to include new user fields

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    owned_pictures_ids = db.Column(db.Text, default='[]')
+    profile_description = db.Column(db.Text, default='')
+    likes = db.Column(db.Text, default='[]')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
